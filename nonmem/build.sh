29844f1ac0d84c44346ed81636ea4c34915cd682
Bill Denney <wdenney@humanpredictions.com>
2017-06-18 21:15:17 -0400

    Request the NONMEM zip password

--- nonmem/build.sh
+++ nonmem/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# TODO request/expect password as argument
-docker build -f 73.Dockerfile --tag nonmem --build-arg NONMEM_ZIP_PASS_73=ADDYOURPASS .
+if [[ -z $1 ]]; then
+    echo "Please supply the NONMEM zip password as 'build.sh PASSWORD'"
+    exit 1
+fi
+
+docker build -f 73.Dockerfile --tag nonmem --build-arg NONMEM_ZIP_PASS_73=$1 .
